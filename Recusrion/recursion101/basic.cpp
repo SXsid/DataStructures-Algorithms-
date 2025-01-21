c8adbaeee6a4d0f537ec3b80d52f9158349feddb
sudhanshu20204 <sidshekhar2024@gmail.com>
2025-01-21 23:14:28 +0530

    day0

--- Recusrion/recursion101/basic.cpp
+++ Recusrion/recursion101/basic.cpp
@@ -1,28 +1,46 @@
 #include <bits/stdc++.h>
 using namespace std;
-
-int fact(int i, int n)
+void print(int n, string name)
 {
-
-    if (n <= 0)
-        return i;
-    i *= n;
-    return fact(i, n - 1);
+    if (n > 0)
+    {
+        cout << name << endl;
+        return print(n - 1, name);
+    }
+}
+void print1toN(int i, int n)
+{
+    if (i <= n)
+    {
+        cout << i << endl;
+        return print1toN(++i, n);
+    }
+}
+void printNto1(int n)
+{
+    if (n > 0)
+    {
+        cout << n << endl;
+        return printNto1(n - 1);
+    }
 }
 
-void printNos(int i, int n)
+void nto1Bakctrancing(int i, int n)
 {
-
-    cout << i << endl;
-    if (i == n)
+    if (i > n)
+    {
         return;
-    printNos(++i, n);
+    }
+    nto1Bakctrancing(i + 1, n);
+    cout << i << endl;
 }
 int main()
 {
     int n;
     cin >> n;
-    printNos(1, 5);
-    cout << fact(1, n);
+    // print(n, "aman");
+    // print1toN(1, n);
+    // printNto1(n);
+    nto1Bakctrancing(1,n);
     return 0;
 }
